7ea4a49d18e7497db5c28c1b4744c77d12f12caa
Salakhutdinov Salavat <salahutdinov.salavat@gmail.com>
2021-09-08 22:18:09 +0300

    remove unnecessary TestCaseTrait

--- src/Abstracts/Tests/PhpUnit/TestCase.php
+++ src/Abstracts/Tests/PhpUnit/TestCase.php
@@ -2,7 +2,6 @@
 
 namespace Laraneat\Core\Abstracts\Tests\PhpUnit;
 
-use Laraneat\Core\Traits\TestCaseTrait;
 use Laraneat\Core\Traits\TestsTraits\PhpUnit\TestsAuthHelperTrait;
 use Laraneat\Core\Traits\TestsTraits\PhpUnit\TestsUrlHelperTrait;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -10,8 +9,7 @@ use Illuminate\Foundation\Testing\TestCase as LaravelTestCase;
 
 abstract class TestCase extends LaravelTestCase
 {
-    use TestCaseTrait,
-        TestsUrlHelperTrait,
+    use TestsUrlHelperTrait,
         TestsAuthHelperTrait,
         RefreshDatabase;
 
@@ -19,11 +17,6 @@ abstract class TestCase extends LaravelTestCase
      * Determine if the seed task should be run when refreshing the database.
      */
     protected bool $seed = true;
-
-    /**
-     * The base URL to use while testing the application.
-     */
-    protected string $baseUrl;
 
     /**
      * Set up the test environment, before each test.
